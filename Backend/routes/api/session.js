aec67dd32f06337481a6d6421d1bcefb7ec9cb0d
Kurisone <wrathscythe@icloud.com>
2025-08-14 19:22:25 -0700

    Phase 4

--- Backend/routes/api/session.js
+++ Backend/routes/api/session.js
@@ -6,8 +6,6 @@ const bcrypt = require('bcryptjs');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
-
-
 const router = express.Router();
 
 
