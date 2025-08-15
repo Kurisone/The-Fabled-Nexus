b592f8891e8c0b36fe0caccbbe6e854e01ea4f16
Kurisone <wrathscythe@icloud.com>
2025-08-14 19:47:32 -0700

    renamed files

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { restoreUser } = require("../../utils/auth.js");
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
-const { User } = require('../../db/models');
+const { User } = require('../../db/models/index.js');
 
 router.use('/session', sessionRouter);
 router.use(restoreUser);
