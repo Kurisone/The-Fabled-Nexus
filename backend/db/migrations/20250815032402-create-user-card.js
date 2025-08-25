ef8b964c73bbdab73de3b211a93b452e9d449a5a
Kurisone <wrathscythe@icloud.com>
2025-08-24 22:06:02 -0700

    fixing render postgres debacle

--- backend/db/migrations/20250815032402-create-user-card.js
+++ backend/db/migrations/20250815032402-create-user-card.js
@@ -13,7 +13,8 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: { model: 'Users', key: 'id' },
-        onDelete: 'CASCADE'
+        onDelete: 'CASCADE',
+        field: 'userId'
       },
       scryfallCardId: { type: Sequelize.STRING, allowNull: false },
       quantity: { type: Sequelize.INTEGER, allowNull: false, defaultValue: 1 },
@@ -21,9 +22,22 @@ module.exports = {
       updatedAt: { allowNull: false, type: Sequelize.DATE, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP') }
     }, options);
 
+    await queryInterface.addConstraint({ tableName: 'UserCards', ...options }, {
+      fields: ['userId'],
+      type: 'foreign key',
+      name: 'usercards_userid_fkey',
+      references: {
+        table: 'Users',
+        field: 'id'
+      },
+      onDelete: 'CASCADE'
+    });
+
+
     await queryInterface.addIndex({ tableName: 'UserCards', ...options }, ['userId']);
   },
   async down(queryInterface, Sequelize) {
+    await queryInterface.removeConstraint({ tableName: 'UserCards', ...options }, 'usercards_userid_fkey');
     await queryInterface.dropTable('UserCards', options);
   }
 };
