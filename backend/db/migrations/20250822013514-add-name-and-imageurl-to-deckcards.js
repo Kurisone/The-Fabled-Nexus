f5595ec05f2b88f632a244ce8d2f0e2d33d1d968
Kurisone <wrathscythe@icloud.com>
2025-08-24 22:43:50 -0700

    fixed migrations

--- backend/db/migrations/20250822013514-add-name-and-imageurl-to-deckcards.js
+++ backend/db/migrations/20250822013514-add-name-and-imageurl-to-deckcards.js
@@ -1,20 +1,26 @@
 'use strict';
 
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+options.tableName = 'DeckCards';
+
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('DeckCards', 'name', {
+    await queryInterface.addColumn(options, 'name', {
       type: Sequelize.STRING,
       allowNull: false,
-      defaultValue: ''  
+      defaultValue: ''
     });
-    await queryInterface.addColumn('DeckCards', 'imageUrl', {
+    await queryInterface.addColumn(options, 'imageUrl', {
       type: Sequelize.STRING,
       allowNull: true
     });
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn('DeckCards', 'name');
-    await queryInterface.removeColumn('DeckCards', 'imageUrl');
+    await queryInterface.removeColumn(options, 'name');
+    await queryInterface.removeColumn(options, 'imageUrl');
   }
 };
